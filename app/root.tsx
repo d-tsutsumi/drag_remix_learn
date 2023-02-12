8e57734d41242705db5b4b4b6de875453d3a7102
大樹 堤 <daiju.tsutsumi@daiju.tsutsumi>
2023-02-12 23:28:17 +0900

    add chakra ui setting

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { Box, ChakraProvider, Heading } from "@chakra-ui/react";
 import type { MetaFunction } from "@remix-run/node";
 import {
   Links,
@@ -6,7 +7,9 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from "@remix-run/react";
+import type { FC, ReactNode } from "react";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
@@ -14,19 +17,76 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export default function App() {
+/**
+ *
+ *  catch boundary (fetch)
+ */
+export const CathchBoundary = () => {
+  const caught = useCatch();
+
+  return (
+    <Document title={`${caught.status} ${caught.statusText}`}>
+      <ChakraProvider>
+        <Box>
+          <Heading as="h1" bg="purple.400">
+            Error catch : {caught.status} : {caught.statusText}
+          </Heading>
+        </Box>
+      </ChakraProvider>
+    </Document>
+  );
+};
+
+/**
+ * error boundary
+ */
+
+type Props = {
+  error: Error;
+};
+export const ErrorBoundary: FC<Props> = ({ error }) => {
+  return (
+    <Document title="Error!">
+      <ChakraProvider>
+        <Box>
+          <Heading as="h1" bg="blue.500">
+            [ErrorBoundary]: There was an error: {error.message}
+          </Heading>
+        </Box>
+      </ChakraProvider>
+    </Document>
+  );
+};
+
+type AppDocumentType = {
+  children: ReactNode;
+  title?: string;
+};
+
+const Document: FC<AppDocumentType> = ({ children, title = "remix drag" }) => {
   return (
     <html lang="en">
       <head>
         <Meta />
+        {title}
         <Links />
       </head>
       <body>
-        <Outlet />
+        {children}
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
+  );
+};
+
+export default function App() {
+  return (
+    <Document>
+      <ChakraProvider>
+        <Outlet />
+      </ChakraProvider>
+    </Document>
   );
 }
