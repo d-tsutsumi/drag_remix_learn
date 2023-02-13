313c57366f1ec44ca51893771c137e114ce5bd92
大樹 堤 <daiju.tsutsumi@daiju.tsutsumi>
2023-02-13 09:14:08 +0900

    update chakra ui setting

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,7 @@
+import type { FC} from "react";
+import { useContext, useEffect } from "react";
+import { withEmotionCache } from "@emotion/react";
 import { Box, ChakraProvider, Heading } from "@chakra-ui/react";
-import type { MetaFunction } from "@remix-run/node";
 import {
   Links,
   LiveReload,
@@ -9,13 +11,79 @@ import {
   ScrollRestoration,
   useCatch,
 } from "@remix-run/react";
-import type { FC, ReactNode } from "react";
+import type { MetaFunction, LinksFunction } from "@remix-run/node"; // Depends on the runtime you choose
+
+import { ServerStyleContext, ClientStyleContext } from "./context/chakra";
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
   viewport: "width=device-width,initial-scale=1",
 });
+
+export let links: LinksFunction = () => {
+  return [
+    { rel: "preconnect", href: "https://fonts.googleapis.com" },
+    { rel: "preconnect", href: "https://fonts.gstatic.com" },
+    {
+      rel: "stylesheet",
+      href: "https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap",
+    },
+  ];
+};
+
+interface DocumentProps {
+  children: React.ReactNode;
+  title?: string;
+}
+
+const Document = withEmotionCache(
+  ({ children, title = "drag remix" }: DocumentProps, emotionCache) => {
+    const serverStyleData = useContext(ServerStyleContext);
+    const clientStyleData = useContext(ClientStyleContext);
+    useEffect(() => {
+      emotionCache.sheet.container = document.head;
+      const tags = emotionCache.sheet.tags;
+      emotionCache.sheet.flush();
+      tags.forEach((tag) => {
+        (emotionCache.sheet as any)._insertTag(tag);
+      });
+      clientStyleData?.reset();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+      <html lang="ja">
+        <head>
+          <Meta />
+          <title>{title}</title>
+          <Links />
+          {serverStyleData?.map(({ key, ids, css }) => (
+            <style
+              key={key}
+              data-emotion={`${key} ${ids.join(" ")}`}
+              dangerouslySetInnerHTML={{ __html: css }}
+            />
+          ))}
+        </head>
+        <body>
+          {children}
+          <ScrollRestoration />
+          <Scripts />
+          <LiveReload />
+        </body>
+      </html>
+    );
+  }
+);
+export default function App() {
+  return (
+    <Document>
+      <ChakraProvider>
+        <Outlet />
+      </ChakraProvider>
+    </Document>
+  );
+}
 
 /**
  *
@@ -57,36 +125,3 @@ export const ErrorBoundary: FC<Props> = ({ error }) => {
     </Document>
   );
 };
-
-type AppDocumentType = {
-  children: ReactNode;
-  title?: string;
-};
-
-const Document: FC<AppDocumentType> = ({ children, title = "remix drag" }) => {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        {title}
-        <Links />
-      </head>
-      <body>
-        {children}
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
-};
-
-export default function App() {
-  return (
-    <Document>
-      <ChakraProvider>
-        <Outlet />
-      </ChakraProvider>
-    </Document>
-  );
-}
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,3 +1,9 @@
+import type { LoaderFunction } from "@remix-run/node";
+
 export default function Index() {
   return <div>Test</div>;
 }
+
+export const loader: LoaderFunction = async ({ request }) => {
+  return request.headers.get("cookie") ?? "";
+};
